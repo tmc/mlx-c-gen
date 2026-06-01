9e10b927fa22bba612373b1c6c51b22e56361dec
Travis Cline <travis.cline@gmail.com>
2026-05-31 21:42:05 -0700

    capi: lock enum conversion values

--- mlx/c/private/enums.h
+++ mlx/c/private/enums.h
@@ -9,6 +9,18 @@
 #include "mlx/mlx.h"
 
 namespace {
+#define MLX_ENUM_ASSERT(c_name, cpp_name)                     \
+  static_assert(                                              \
+      static_cast<int>(c_name) == static_cast<int>(cpp_name), \
+      "MLX enum value mismatch: " #c_name)
+
+MLX_ENUM_ASSERT(MLX_COMPILE_MODE_DISABLED, mlx::core::CompileMode::disabled);
+MLX_ENUM_ASSERT(
+    MLX_COMPILE_MODE_NO_SIMPLIFY,
+    mlx::core::CompileMode::no_simplify);
+MLX_ENUM_ASSERT(MLX_COMPILE_MODE_NO_FUSE, mlx::core::CompileMode::no_fuse);
+MLX_ENUM_ASSERT(MLX_COMPILE_MODE_ENABLED, mlx::core::CompileMode::enabled);
+
 inline mlx_compile_mode mlx_compile_mode_to_c(mlx::core::CompileMode type) {
   static mlx_compile_mode map[] = {
       MLX_COMPILE_MODE_DISABLED,
@@ -25,6 +37,22 @@ inline mlx::core::CompileMode mlx_compile_mode_to_cpp(mlx_compile_mode type) {
       mlx::core::CompileMode::enabled};
   return map[(int)type];
 }
+
+MLX_ENUM_ASSERT(MLX_BOOL, mlx::core::Dtype::Val::bool_);
+MLX_ENUM_ASSERT(MLX_UINT8, mlx::core::Dtype::Val::uint8);
+MLX_ENUM_ASSERT(MLX_UINT16, mlx::core::Dtype::Val::uint16);
+MLX_ENUM_ASSERT(MLX_UINT32, mlx::core::Dtype::Val::uint32);
+MLX_ENUM_ASSERT(MLX_UINT64, mlx::core::Dtype::Val::uint64);
+MLX_ENUM_ASSERT(MLX_INT8, mlx::core::Dtype::Val::int8);
+MLX_ENUM_ASSERT(MLX_INT16, mlx::core::Dtype::Val::int16);
+MLX_ENUM_ASSERT(MLX_INT32, mlx::core::Dtype::Val::int32);
+MLX_ENUM_ASSERT(MLX_INT64, mlx::core::Dtype::Val::int64);
+MLX_ENUM_ASSERT(MLX_FLOAT16, mlx::core::Dtype::Val::float16);
+MLX_ENUM_ASSERT(MLX_FLOAT32, mlx::core::Dtype::Val::float32);
+MLX_ENUM_ASSERT(MLX_FLOAT64, mlx::core::Dtype::Val::float64);
+MLX_ENUM_ASSERT(MLX_BFLOAT16, mlx::core::Dtype::Val::bfloat16);
+MLX_ENUM_ASSERT(MLX_COMPLEX64, mlx::core::Dtype::Val::complex64);
+
 inline mlx_dtype mlx_dtype_to_c(mlx::core::Dtype type) {
   static mlx_dtype map[] = {
       MLX_BOOL,
@@ -63,6 +91,10 @@ inline mlx::core::Dtype mlx_dtype_to_cpp(mlx_dtype type) {
   };
   return map[(int)type];
 }
+
+MLX_ENUM_ASSERT(MLX_CPU, mlx::core::Device::DeviceType::cpu);
+MLX_ENUM_ASSERT(MLX_GPU, mlx::core::Device::DeviceType::gpu);
+
 inline mlx_device_type mlx_device_type_to_c(
     mlx::core::Device::DeviceType type) {
   static mlx_device_type map[] = {MLX_CPU, MLX_GPU};
@@ -74,6 +106,11 @@ inline mlx::core::Device::DeviceType mlx_device_type_to_cpp(
       mlx::core::Device::DeviceType::cpu, mlx::core::Device::DeviceType::gpu};
   return map[(int)type];
 }
+
+MLX_ENUM_ASSERT(MLX_FFT_NORM_BACKWARD, mlx::core::fft::FFTNorm::Backward);
+MLX_ENUM_ASSERT(MLX_FFT_NORM_ORTHO, mlx::core::fft::FFTNorm::Ortho);
+MLX_ENUM_ASSERT(MLX_FFT_NORM_FORWARD, mlx::core::fft::FFTNorm::Forward);
+
 inline mlx_fft_norm mlx_fft_norm_to_c(mlx::core::fft::FFTNorm norm) {
   static mlx_fft_norm map[] = {
       MLX_FFT_NORM_BACKWARD, MLX_FFT_NORM_ORTHO, MLX_FFT_NORM_FORWARD};
@@ -86,6 +123,8 @@ inline mlx::core::fft::FFTNorm mlx_fft_norm_to_cpp(mlx_fft_norm norm) {
       mlx::core::fft::FFTNorm::Forward};
   return map[(int)norm];
 }
+
+#undef MLX_ENUM_ASSERT
 } // namespace
 
 #endif
